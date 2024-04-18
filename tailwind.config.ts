6f77ae2cb9c5d4afc92aa08d2914939e73d6e4f6
JoseMM2002 <jeolmmt@gmail.com>
2024-04-17 21:44:28 -0600

    theme update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
     content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+            'credit-types': "url('/src/assets/creditcard_sprite.png')"
+        }
+      },
+      colors: {
+        primary: '#9b85ad',
+        neutral: {
+            0: '#e1e1d6',
+            1: '#D4D4CE',
+            2: '#B9B9B9',
+            3: '#909090',
+        },
+        white: '#f1f0e7',
+        blue: {
+            1: '#73ABCF',
+            2: '#126cae'
+        }
+      }
     },
     plugins: [],
   }
