52a862e16479073bbbafda429e87579045d6d41a
JoseMM2002 <jeolmmt@gmail.com>
2024-04-18 18:57:30 -0600

    fixing some styling and clases

--- src/components/CardTypeIndicator.tsx
+++ src/components/CardTypeIndicator.tsx
@@ -3,21 +3,19 @@ import type { CardType } from "src/constants/CardTypes";
 const CardTypeIndicator: React.FC<{ cardType: CardType }> = ({ cardType }) => {
     const positioning: Record<CardType, string> = {
         NONE: "w-0",
-        STAR: "w-[29px] right-0 credit-card-positioning star-positioning",
-        PULSE: "w-[45px] left-[38px] credit-card-positioning pulse-positioning",
-        MAESTRO:
-            "w-[35px] left-[90px] credit-card-positioning maestro-positioning",
-        MASTERCARD:
-            "w-[32px] right-[80px] credit-card-positioning mastercard-positioning",
-        PLUS: "w-[29px] right-[35px] credit-card-positioning plus-positioning",
-        VISA: "w-[29px] right-0 credit-card-positioning visa-positioning",
+        STAR: "w-[29px] left-0 star-positioning",
+        PULSE: "w-[45px] left-[38px] pulse-positioning",
+        MAESTRO: "w-[35px] left-[90px] maestro-positioning",
+        MASTERCARD: "w-[32px] right-[80px] mastercard-positioning",
+        PLUS: "w-[29px] right-[35px] plus-positioning",
+        VISA: "w-[29px] right-0 visa-positioning",
     };
     const cardPositioning = positioning[cardType];
     return (
         <>
             <div className="bg-credit-types w-[238px] h-[22px] bg-bottom relative">
                 <div
-                    className={`absolute bg-credit-types top-0 bottom-0 ${cardPositioning}`}
+                    className={`absolute bg-credit-types bottom-0 h-[19px] ${cardPositioning}`}
                 ></div>
             </div>
         </>
