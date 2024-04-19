45628c4fcee309b04597488e91129279cd9d9fa1
JoseMM2002 <jeolmmt@gmail.com>
2024-04-18 19:24:51 -0600

    https support

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: 'frontend.hw.project',
+    port: 8080,
+    https: {
+      cert: 'frontend.hw.project.pem',
+      key: 'frontend.hw.project-key.pem',
+    },
+  },
   plugins: [react()],
 })
