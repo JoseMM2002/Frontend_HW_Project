9d7179379221f0af5b5d3e6ed74acfb5e8b599b5
JoseMM2002 <jeolmmt@gmail.com>
2024-04-20 21:22:39 -0600

    adding input for atm

--- src/components/AtmDisplay.tsx
+++ src/components/AtmDisplay.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import TerminalText from "./TerminalText";
 import type { AtmOptionProps } from "./AtmOption";
 import AtmOption from "./AtmOption";
@@ -9,13 +9,23 @@ export type AtmDisplayOption = Omit<AtmOptionProps, "position">;
 interface AtmDisplayProps {
     options: Omit<AtmOptionProps, "position">[];
     title: string;
+    currentInput: string;
+    onInputChange?: (input: string) => void;
 }
 
-const AtmDisplay: React.FC<AtmDisplayProps> = ({ options, title }) => {
+const AtmDisplay: React.FC<AtmDisplayProps> = ({
+    options,
+    title,
+    onInputChange,
+    currentInput,
+}) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
     const distributedOptions: {
         left: AtmDisplayOption;
         right: AtmDisplayOption;
     }[] = [];
+
     for (let i = 0; i < 4; i++) {
         distributedOptions.push({
             right: options[i * 2] || {
@@ -28,12 +38,41 @@ const AtmDisplay: React.FC<AtmDisplayProps> = ({ options, title }) => {
             },
         });
     }
+
+    const focusInput = () => {
+        if (inputRef.current) inputRef.current.focus();
+    };
+
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!onInputChange) return;
+        const value = e.target.value;
+
+        onInputChange(value);
+    };
+
+    useEffect(() => {
+        if (inputRef.current) inputRef.current.value = currentInput;
+    }, [currentInput]);
+
     return (
         <>
-            <div className="bg-blue-1 border-4 border-neutral-1 pt-3 px-3 pb-1 w-full h-60 font-atm text-white text-xl flex flex-col">
+            <div className="bg-blue-1 border-4 border-neutral-1 pt-3 px-3 pb-1 w-full h-72 font-atm text-white flex flex-col">
                 <TerminalText
                     text={title}
-                    className="text-center"
+                    className="text-center h-12 text-lg"
+                    focus={onInputChange ? false : true}
+                ></TerminalText>
+                <input
+                    type="text"
+                    ref={inputRef}
+                    style={{ position: "absolute", top: "-9999px" }}
+                    onChange={handleInput}
+                ></input>
+                <TerminalText
+                    text={currentInput}
+                    onClick={focusInput}
+                    focus={onInputChange ? true : false}
+                    className="text-left h-10"
                 ></TerminalText>
                 <div className="flex-1 flex flex-col-reverse text-xs">
                     {distributedOptions.map(({ left, right }, idx) => (
