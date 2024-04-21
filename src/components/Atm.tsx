b64ee23020c3bf74b373d7fa63f519a95dd2ad3c
JoseMM2002 <jeolmmt@gmail.com>
2024-04-20 21:23:06 -0600

    using AtmState in atm component

--- src/components/Atm.tsx
+++ src/components/Atm.tsx
@@ -1,35 +1,31 @@
 import AtmSign from "../assets/atm_sign.png";
 import Graffiti from "../assets/graffiti.png";
-import type { AtmDisplayOption } from "./AtmDisplay";
 import AtmDisplay from "./AtmDisplay";
-import CardTypeIndicator from "./CardTypeIndicator";
 import StickerGraffiti from "../assets/sticker_graf.png";
+import { useEffect, useState } from "react";
+import AccountTypeIndicator from "./AccountTypeIndicator";
+import { AtmState, createInstance } from "../atm/index";
 
 function Atm() {
-    const options: AtmDisplayOption[] = [
-        {
-            option: "Enter PIN",
-            callback: async () => {
-                console.log("this is a callback");
-            },
-        },
-        {
-            option: "Withdraw",
-            callback: async () => {
-                console.log("this is a callback");
-            },
-        },
-        {
-            option: "Balance",
-            callback: async () => {
-                console.log("this is a callback");
-            },
-        },
-    ];
+    const [atmState, setAtmState] = useState<AtmState | null>(null);
+    useEffect(() => {
+        const initialize = async () => {
+            const initialState = await createInstance(setAtmState);
+            setAtmState(initialState);
+        };
+        initialize();
+    }, []);
+
+    const [inputState, setInput] = useState("");
+
+    useEffect(() => {
+        setInput(atmState?.input || "");
+    }, [atmState?.input]);
+
     return (
         <>
             <div className="h-full flex flex-col items-center relative">
-                <div className="bg-blue-2 py-4 px-16 rounded-[14px] relative">
+                <div className="bg-blue-2 py-4 px-20 rounded-[14px] relative">
                     <img src={AtmSign} alt="Atm Sign" />
                     <img
                         src={Graffiti}
@@ -37,15 +33,25 @@ function Atm() {
                         className="absolute right-[28px] top-1/4"
                     />
                 </div>
-                <div className="flex-1 w-[85%]">
+                <div className="flex-1 w-[90%]">
                     <div className="bg-white h-full border-t-[15px] border-neutral-2 w-full flex flex-col items-center">
                         <div className="py-2">
-                            <CardTypeIndicator cardType="VISA"></CardTypeIndicator>
+                            <AccountTypeIndicator accountType="VISA"></AccountTypeIndicator>
                         </div>
-                        <div className="w-[65%]">
+                        <div className="w-[70%]">
                             <AtmDisplay
-                                options={options}
-                                title="Welcome to the ATM"
+                                options={atmState?.options || []}
+                                title={atmState?.title || ""}
+                                onInputChange={
+                                    atmState?.setInput
+                                        ? (input) => {
+                                              if (atmState.setInput)
+                                                  atmState.setInput(input);
+                                              setInput(atmState.input);
+                                          }
+                                        : undefined
+                                }
+                                currentInput={inputState}
                             ></AtmDisplay>
                         </div>
                         <img
