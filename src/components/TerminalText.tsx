c32701bfab64999afbeb87b0196904ce87845186
JoseMM2002 <jeolmmt@gmail.com>
2024-04-20 21:14:20 -0600

    terminal text able to render \n chars

--- src/components/TerminalText.tsx
+++ src/components/TerminalText.tsx
@@ -1,22 +1,35 @@
 import React, { useState, useEffect } from "react";
 
-const TerminalText: React.FC<{ text: string; className?: string }> = ({
-    text,
-    className,
-}) => {
+const TerminalText: React.FC<{
+    text: string;
+    focus: boolean;
+    className?: string;
+    onClick?: () => void;
+}> = ({ text, focus, ...props }) => {
     const [showCursor, setShowCursor] = useState(true);
 
     useEffect(() => {
-        const timer = setInterval(() => {
-            setShowCursor((prevShowCursor) => !prevShowCursor);
-        }, 500);
+        if (focus) {
+            const timer = setInterval(() => {
+                setShowCursor((prevShowCursor) => !prevShowCursor);
+            }, 500);
 
-        return () => clearInterval(timer);
-    }, []);
+            return () => clearInterval(timer);
+        } else {
+            setShowCursor(false);
+        }
+    }, [focus]);
+
+    const lines = text.split("\n");
 
     return (
-        <div className={className}>
-            {text}
+        <div {...props}>
+            {lines.map((line, index) => (
+                <React.Fragment key={index}>
+                    {line} {}
+                    {index < lines.length - 1 && <br />}
+                </React.Fragment>
+            ))}
             {showCursor && <span className="cursor">|</span>}
         </div>
     );
